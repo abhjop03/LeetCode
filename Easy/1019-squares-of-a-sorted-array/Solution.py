54c58dbaf3c691e36bf01205ec2b4b9b568a6a8c
Abhishek Yadav <115218440+abhjop03@users.noreply.github.com>
2026-09-22 18:23:35 +0530

    ✅ [Easy] #1019 - Squares of a Sorted Array | Python3 | Runtime: 12 ms | Memory: 21.1 MB

--- Easy/1019-squares-of-a-sorted-array/Solution.py
+++ Easy/1019-squares-of-a-sorted-array/Solution.py
@@ -1,42 +1,15 @@
 class Solution:
     def sortedSquares(self, nums: list[int]) -> list[int]:
-        nums1 = []
-        nums2 = []
-        for num in nums:
-            if num < 0:
-                nums1.append(num*num)
+        n = len(nums)
+        result = [0]*n
+        left = 0
+        right = n-1
+
+        for i in range(n-1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+                result[i] = nums[left] ** 2
+                left+=1
             else:
-                nums2.append(num*num)
-        nums1.reverse()
-
-        idx1 = 0
-        idx2 = 0
-        count = 0
-
-        while idx1 < len(nums1) and idx2 < len(nums2):
-            if nums1[idx1] < nums2[idx2] and idx1 < len(nums1):
-                nums[count] = nums1[idx1]
-                idx1+=1
-            elif nums2[idx2] < nums1[idx1] and idx2 < len(nums2):
-                nums[count] = nums2[idx2]
-                idx2+=1
-            elif nums1[idx1] == nums2[idx2]:
-                nums[count] = nums1[idx1]
-                idx1+=1
-                count+=1
-                nums[count] = nums2[idx2]
-                idx2+=1
-            count+=1
-
-        while idx1 < len(nums1):
-            nums[count] = nums1[idx1]
-            idx1+=1
-            count+=1
-
-        while idx2 < len(nums2):
-            nums[count] = nums2[idx2]
-            idx2+=1
-            count+=1
-            
-        
-        return nums
+                result[i] = nums[right] ** 2
+                right-=1
+        return result
